55981320590c5fd8550eaed964659bce31bb1fa1
ankman007 <ankeetpoudel6@gmail.com>
2025-06-15 11:30:29 +0545

    redis-integration

--- a_core/settings.py
+++ a_core/settings.py
@@ -67,11 +67,21 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'a_core.asgi.application'
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
+
 CHANNEL_LAYERS = {
     'default': {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
 }
+
 
 DATABASES = {
     'default': {
