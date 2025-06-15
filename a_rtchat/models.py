b8b0792951ee2f3adce106abc0da67d17f39292f
ankman007 <ankeetpoudel6@gmail.com>
2025-06-15 10:58:12 +0545

    online-tracker-functionality-update

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True) 
     
     def __str__(self):
         return self.group_name
@@ -19,4 +20,3 @@ class GroupMessage(models.Model):
     
     class Meta:
         ordering = ['-created']
-    
